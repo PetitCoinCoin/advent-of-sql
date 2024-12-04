d3f72c26e75696abfd4a83cd325bef3a1a7d5010
Andréa <andrea.marnier@gmail.com>
2024-12-04 09:47:35 +0100

    day 04 update with cross join

--- day_04.sql
+++ day_04.sql
@@ -20,3 +20,37 @@ SELECT
 FROM toy_production
 ORDER BY added_tags DESC NULLS LAST
 LIMIT 1;
+
+-- This works as well
+SELECT *
+FROM (
+    SELECT
+        toy_p.toy_id,
+        array_length(added.added_tags, 1) AS added_tags,
+        array_length(unchanged.unchanged_tags, 1) AS unchanged_tags,
+        array_length(removed.removed_tags, 1) AS removed_tags
+    FROM toy_production toy_p
+    CROSS JOIN LATERAL (
+    SELECT ARRAY (
+        SELECT unnest(toy_p."new_tags")
+        EXCEPT ALL
+        SELECT unnest(toy_p."previous_tags")
+        )
+    ) added(added_tags)
+    CROSS JOIN LATERAL (
+    SELECT ARRAY (
+        SELECT unnest(toy_p."previous_tags")
+        EXCEPT ALL
+        SELECT unnest(toy_p."new_tags")
+        )
+    ) removed(removed_tags)
+    CROSS JOIN LATERAL (
+    SELECT ARRAY (
+        SELECT unnest(toy_p."new_tags")
+        INTERSECT
+        SELECT unnest(toy_p."previous_tags")
+        )
+    ) unchanged(unchanged_tags)
+)
+ORDER BY added_tags DESC NULLS LAST
+LIMIT 1;
